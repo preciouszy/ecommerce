33fb828fad92db9144366eb6663d19c0ee701ed6
preciouszy <preciousd533@gmail.com>
2022-01-10 09:30:20 -0800

    publist to heroku

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,7 @@ import productRouter from './routers/productRouter.js'
 import userRouter from './routers/userRouter.js'
 import orderRouter from './routers/orderRouter.js'
 import uploadRouter from './routers/uploadRouter.js'
-import path from 'path';
-
-
+import path from 'path'
 
 dotenv.config()
 const app = express()
@@ -15,21 +13,24 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/ecommerce')
-app.use('/api/uploads', uploadRouter);
+app.use('/api/uploads', uploadRouter)
 app.use('/api/users', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
 app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-});
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-app.get('/', (req, res) => {
-  res.send('Server is ready')
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
 })
-app.use((err,req,res,next)=>{
-  res.status(500).send({message: err.message})
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+)
+// app.get('/', (req, res) => {
+//   res.send('Server is ready')
+// })
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message })
 })
 const port = process.env.PORT || 5000
 app.listen(port, () => {
